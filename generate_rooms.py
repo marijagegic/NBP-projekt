d11d5c2b7b4034736905484c6ac044cb35a0ebd5
MDBcode <benkovic.md@gmail.com>
2022-06-14 01:09:58 +0200

    refactoring

--- generate_rooms.py
+++ generate_rooms.py
@@ -5,13 +5,13 @@ import pandas as pd
 #df_hotels = pd.read_csv("hoteli.csv")
 #stars = df_hotels["stars"]
 
-stars = [np.random.randint(3,6) for _ in range(6*10)]  # dummy podaci
+stars = [np.random.randint(3,6) for _ in range(6*10)]  # dummy podaci, zvjezdice hotela
 
 avg_daily_price = 103.25 # u dolarima - podatak iz 2020.godine
 
 if __name__ == "__main__":
     df = pd.DataFrame()
-    for star in stars:
+    for star in stars:  # za svaki hotel s tom zvjezdicom -> i treba još puta broj soba(možda 3?)
         beds = np.random.normal(2, 0.5)  # cca 1-4 kreveta, najcesce 2 i 3
         beds = int(np.ceil(beds))
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
         # TODO u hoteli.csv dodati cjenik: cca 0 za polupansion, 50 za puni, 100 za all_inclusive (ali ovisi i o broju zvjezdica)
 
-        df2 = {"beds": beds, "daily_price": int(daily_price), "stars":star}
+        df2 = {"stars":star, "beds": beds, "daily_price": int(daily_price), "occupied": 0}
         df = df.append(df2, ignore_index=True)
 
     print(df)
