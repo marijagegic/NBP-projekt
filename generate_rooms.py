c07944ced2ccfc2a56150027df0ed71f709f74e3
MDBcode <benkovic.md@gmail.com>
2022-06-14 16:28:31 +0200

    dodani hoteli i sobe

--- generate_rooms.py
+++ generate_rooms.py
@@ -1,29 +1,41 @@
 import numpy as np
 import pandas as pd
+import random
 
-""" ovo odkomentirat kad se kreiraju hoteli"""
-#df_hotels = pd.read_csv("hoteli.csv")
-#stars = df_hotels["stars"]
+df_hotels = pd.read_csv("hotels_v2.csv")
+stars = df_hotels["stars"]
 
-stars = [np.random.randint(3,6) for _ in range(6*10)]  # dummy podaci, zvjezdice hotela
-
-avg_daily_price = 103.25 # u dolarima - podatak iz 2020.godine
+avg_daily_price = 103.25  # u dolarima - podatak iz 2020.godine
 
 if __name__ == "__main__":
-    df = pd.DataFrame()
-    for star in stars:  # za svaki hotel s tom zvjezdicom -> i treba još puta broj soba(možda 3?)
-        beds = np.random.normal(2, 0.5)  # cca 1-4 kreveta, najcesce 2 i 3
-        beds = int(np.ceil(beds))
+    df_rooms = pd.DataFrame()
 
+    # u dolarima, +10 za half, +30 za full, +50 za all_inclusive --- ovisi i o broju zvjezdica hotela
+    if "half_board" not in df_hotels:
+        df_hotels["half_board"] = np.round((stars-2)*10 + np.random.normal(0, 1),1)
+    if "full_board" not in df_hotels:
+        df_hotels["full_board"] = np.round((stars-2)*20 + np.random.normal(0, 2),1)
+    if "all_inclusive" not in df_hotels:
+        df_hotels["all_inclusive"] = np.round((stars-2)*30 + np.random.normal(0, 3),1)
+
+    df_hotels.to_csv("hotels_v3.csv", index=False)
+
+    for row in df_hotels.itertuples():  # prodi po retcima=hotelima
+        city = row[1]
+        hotel_name = row[2]
+        star = row[3]
         plus_minus = np.random.normal(0, 10)  # centar je 0, stand. dev = 10
-        # avg_daily_price je pocetna cijena za kategoriju 3 zvijezdice i +200 dolara za kategoriju vise
-        # plus 30 dolara za dodatni krevet
-        daily_price = avg_daily_price + (star-3)*200 + (beds-1)*30 + plus_minus
+        for i in range(10):  # 10 soba za svaki hotel
+            beds = np.random.normal(2, 0.5)  # cca 1-4 kreveta, najcesce 2 i 3
+            beds = int(np.ceil(beds))
 
-        # TODO u hoteli.csv dodati cjenik: cca 0 za polupansion, 50 za puni, 100 za all_inclusive (ali ovisi i o broju zvjezdica)
+            # avg_daily_price je pocetna cijena za kategoriju 3 zvijezdice i +200 dolara za kategoriju vise
+            # 1 krevet nema dodatne naknade, 2 kreveta=+30$, 3 kreveta=+60$ itd.
+            # plus 30 dolara za dodatni krevet
+            daily_price = avg_daily_price + (star - 3) * 200 + (beds - 1) * 30 + plus_minus
 
-        df2 = {"stars":star, "beds": beds, "daily_price": int(daily_price), "occupied": 0}
-        df = df.append(df2, ignore_index=True)
+            room = {"city":city, "hotel_name": hotel_name,"beds": beds, "daily_price": np.round(daily_price,1), "occupied": 0}
+            df_rooms = df_rooms.append(room, ignore_index=True)
 
-    print(df)
-    #df.to_csv("rooms.csv", index=False)
+    # print(df_rooms.head(20), plus_minus)
+    df_rooms.to_csv("rooms.csv", index=False)
